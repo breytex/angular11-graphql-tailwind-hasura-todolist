0e8d70506b0845142cff3b4736776ff338797a9d
breytex <breytex@gmail.com>
2021-04-22 10:05:06 +0200

    Added css pruge for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+const enablePurge = process.env.ENABLE_PURGE || false;
 module.exports = {
   purge: {
-    enabled: false,
+    enabled: enablePurge,
     content: [
       './src/**/*.html',
       './src/**/*.scss'
